50597d4d793118d34de0a7a84d095edb7c386603
twinturbotom <tommmello@gmail.com>
2013-01-25 16:45:55 -0500

    open sum file, write output, find sections

---
+++ summary_file_master_parser.rb
@@ -0,0 +1,15 @@
+sum_file = File.new('sum_file.sum', 'r')
+sum_file_hashed = File.new('sum_file_hashed', 'w')
+
+	while (line = sum_file.gets )
+		case line
+		when /\*{3}/ #Find Sections by ***
+			l = line.gsub('*', '').strip
+
+			sum_file_hashed.puts('Found a section: ' + l )
+		end
+
+	end
+
+sum_file.close
+sum_file_hashed.close
