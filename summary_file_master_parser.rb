f469922657681ba3aeeddab65c8f6d167a68d937
twinturbotom <tommmello@gmail.com>
2013-01-25 17:41:59 -0500

    find section, put in a section

--- summary_file_master_parser.rb
+++ summary_file_master_parser.rb
@@ -1,13 +1,22 @@
 sum_file = File.new('sum_file.sum', 'r')
 sum_file_hashed = File.new('sum_file_hashed', 'w')
 
+inSection = false
+
 	while (line = sum_file.gets )
 		case line
 		when /\*{3}/ #Find Sections by ***
+			inSection = true
 			l = line.gsub('*', '').strip
-
 			sum_file_hashed.puts('Found a section: ' + l )
+
 		end
+	if inSection == true
+		sum_file_hashed.puts("in section")
+	end
+		
+
+
 
 	end
 
