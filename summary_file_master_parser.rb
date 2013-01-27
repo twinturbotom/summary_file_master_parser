d88c928bf03ea86c0ace661f64d92e0b478978bd
twinturbotom <tommmello@gmail.com>
2013-01-27 15:55:19 -0500

    file output sum_section parsed

--- summary_file_master_parser.rb
+++ summary_file_master_parser.rb
@@ -11,17 +11,20 @@ sum_section = {}
 blocks.each do |block|
   case block.shift
   when /Simulation CFD Summary File Output/
-  	#sum_file_hashed.write(block)
-  	#hash[:sim_sum_out] = Hash[block.map{ |r| r.split(/\=/) }]
-  	sva = block[0].gsub(/[\[\]]/,'').split( /[\(\)]/ ).collect(&:strip)
-  	sum_section[:sim_version] = {name: sva[0], code_name: sva[1], date_code: sva[2] } 
-  	sum_file_hashed.puts( sum_section[:sim_version] )
-  	
+  	sim_ver_arr = block[0].gsub(/[\[\]]/,'').split( /[\(\)]/ ).collect(&:strip )
+  	sum_section[:sim_version] = {name: sim_ver_arr[0], code_name: sim_ver_arr[1], date_code: sim_ver_arr[2] }
+  	sda = block[1].split(/\s{3}/)
+  	jn = sda[0].split(/\=/).collect(&:strip)
+  	sum_section[:job_name] = jn[1]
+  	dc = sda[1].split(': ').collect(&:strip)
+  	sum_section[:date_created] = dc[1]
 
+  	sum_file_hashed.puts( sum_section )
+  	
   when /Analysis Information/
     # process the analysis information
     sum_section[:analysis_information] = Hash[block.map{ |r| r.split(/ +is +/) }]
-    sum_file_hashed.puts(sum_section[:analysis_information])
+    #sum_file_hashed.puts(sum_section[:analysis_information])
 
   when /Field Variable Results/
     # process the field variable results
